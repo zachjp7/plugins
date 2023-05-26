9ea5615e3d4def0cc07fc1b2b1814d2cef82dfce
zachjp7 <zachjp7@gmail.com>
2023-05-26 14:57:22 -0500

    Adding Chamber Plugin File
    Chamber Plugin Added

---
+++ custom-chamber/custom-chamber.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Plugin Name: Custom Chamber Plugin
+Plugin URI: 
+Description: This is a custom plugin to help with layout of the chamber members.
+Author: Zach Parkhurst
+Author URI: 
+Version: 1.0
+*/
+
+add_filter( 'theme_page_templates', 'pt_add_page_template_to_dropdown' );
+
+/**
+* Add page templates.
+*
+* @param  array  $templates  The list of page templates
+*
+* @return array  $templates  The modified list of page templates
+*/
+function sf_add_page_template_to_dropdown( $templates )
+{
+   $templates[plugin_dir_path( __FILE__ ) . 'templates/custom-member-template.php'] = __( 'Page Template From Plugin', 'text-domain' );
+
+   return $templates;
+}
